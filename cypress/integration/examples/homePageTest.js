2be536ac0d3447816acf3d13e7de6bdaf2ae5f72
Satya Kalyan <satyakalyan5@gmail.com>
2020-03-01 00:51:52 -0500

    Finally could handle the alert. Yayy! First test case working.

--- cypress/integration/examples/homePageTest.js
+++ cypress/integration/examples/homePageTest.js
@@ -1,21 +1,21 @@
 /// <reference types="Cypress" />
 describe('Started testing the home page for the QA Click Academy page', function(){
-    it("Handling alert page", function(){
+    it("Closing the alert by clicking on 'NO THANKS'", function(){
         cy.visit("http://www.qaclickacademy.com/");
+        //
+        // var alerted = false;
+        // cy.on('window:alert', msg => alerted = msg);
 
-        var alerted = false;
-        cy.on('window:alert', msg => alerted = msg);
-
-        cy.get('button').contains('Subscribe Now').click()
-            .then( () => expect(alerted).to.match(/clicked!/));
-
-        //     (alertString) => {
-        //     //Mocha assertion
-        //     expect(alertString).to.equal("Join Our Newsletter!");
-        // })
-
-        // cy.on('window:confirm', (confirmString) => {
-        //     expect(confirmString).to.equal("Hello , Are you sure you want to confirm?");
+        cy.get('.sumome-react-wysiwyg-move-handle').contains('NO THANKS').click();
+            // .then( () => expect(alerted).to.match(/clicked!/));
+        //
+        //         //     (alertString) => {
+        //         //     //Mocha assertion
+        //         //     expect(alertString).to.equal("Join Our Newsletter!");
+        //         // })
+        //
+        //         // cy.on('window:confirm', (confirmString) => {
+        //         //     expect(confirmString).to.equal("Hello , Are you sure you want to confirm?");
         // })
 
         })

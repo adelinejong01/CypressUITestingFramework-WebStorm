c3a839cb0d4cc01e55b1762945be10a783501d4a
Satya Kalyan <satyakalyan5@gmail.com>
2020-03-07 14:38:46 -0500

    Added the testcase which finds the Text - Featured Courses

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-#Cypress_Learning
-<<<<<<< HEAD
-Trying to figure out how to revert to the penultimate commit
-=======
 # CypressUITestingFramework-WebStorm
->>>>>>> b0bf61e5d6b38bc1aaa653addf898767732b059e
--- cypress/integration/examples/homePageTest.js
+++ cypress/integration/examples/homePageTest.js
@@ -1,22 +1,32 @@
-/// <reference types="Cypress" />
+///// <reference types="Cypress" />
 describe('Started testing the home page for the QA Click Academy page', function(){
+    //This TC is to test the functionality of closing the alert
+
     it("Closing the alert by clicking on 'NO THANKS'", function(){
+        //Navigating to the home page
         cy.visit("http://www.qaclickacademy.com/");
-        //
-        // var alerted = false;
-        // cy.on('window:alert', msg => alerted = msg);
 
+        //Clicking on the NO THANKS button
         cy.get('.sumome-react-wysiwyg-move-handle').contains('NO THANKS').click();
-            // .then( () => expect(alerted).to.match(/clicked!/));
-        //
-        //         //     (alertString) => {
-        //         //     //Mocha assertion
-        //         //     expect(alertString).to.equal("Join Our Newsletter!");
-        //         // })
-        //
-        //         // cy.on('window:confirm', (confirmString) => {
-        //         //     expect(confirmString).to.equal("Hello , Are you sure you want to confirm?");
-        // })
+    })
 
-        })
+    //This test is for validating that the text "Featured Courses" can be seen on the page
+
+    it("Handling the title text of the page", function () {
+        //Navigating to the home page
+        cy.visit("http://www.qaclickacademy.com/");
+
+        cy.get('.container > .text-center > h2').contains("Featured Courses");
+    })
+
+    //This TC is to verify the error message after clicking 'Subscribe Now' without providing the email id
+
+    // it("Handling the alert by subscribing to it", function () {
+    //     //Navigating to the home page
+    //     cy.visit("http://www.qaclickacademy.com/");
+    //
+    //     cy.get('.sumome-react-wysiwyg-move-handle').contains('Subscribe Now').click().
+    //         then(cy.get('.listbuilder-popup-field-error').to.match("required"));
+    // })
+
 })

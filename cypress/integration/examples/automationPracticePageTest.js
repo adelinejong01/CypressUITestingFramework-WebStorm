75810a2b4e6f5df94935cd517860234ba5174407
Satya Kalyan <satyakalyan5@gmail.com>
2020-03-18 22:23:51 -0400

    Added the Switch to Alert functionality in the automationPracticePage test page

--- cypress/integration/examples/automationPracticePageTest.js
+++ cypress/integration/examples/automationPracticePageTest.js
@@ -163,4 +163,38 @@ describe("Test 2: Running tests on the Automation Practice Page", function(){
         cy.go('back');
     })
 
+    //This test is to test Alert functionality
+    it("Switch To Alert Functionality", function(){
+        //Navigating to the automation practice page
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+        //We are selecting the many titles and filtering the 'Switch To Alert Example' title out of them
+        cy.get('legend').each(($el, index, $list) => {
+            const suggestionClassTitle = $el.text();
+
+            if(suggestionClassTitle.includes('Switch To Alert Example')){
+                cy.log("Found the Switch Tab Example title bar");
+            }
+        })
+
+        //Checking that the text box where we enter the text for the alert exist.
+        //Because checking the attribute of an element is not directly possible, at least to my knowledge, in Cypress, we have divided it into two parts
+        cy.get('input[name = "enter-name"]').should('have.attr', "placeholder");
+
+        //Here is the second part where we are checking the placeholder text is "Enter Your Name" exists
+        cy.get('input[placeholder = "Enter Your Name"]').should('exist');
+
+        //Entering the text in the Alert Box
+        cy.get('input[placeholder = "Enter Your Name"]').type("Batman");
+
+        //Clicking the 'Alert' button
+        cy.get('#alertbtn').click();
+
+        //Checking that the alert text is what it is supposed to be
+        cy.on('window:alert', (alertString) => {
+            //This is a Mocha assertion
+            expect(alertString).to.equal("Hello Batman, share this practice page and share your knowledge");
+        })
+    })
+
 })

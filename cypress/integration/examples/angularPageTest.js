178d25a514d52956f838e1f2885cfc7891857221
Satya Kalyan <satyakalyan5@gmail.com>
2020-03-24 21:09:04 -0400

    Stuck at an issue as to how to handle the DOB field in the Angular Page

--- cypress/integration/examples/angularPageTest.js
+++ cypress/integration/examples/angularPageTest.js
@@ -14,5 +14,28 @@ describe("Testing Angular Page to check Cypress functionality", function () {
         cy.get(':nth-child(2) > .nav-link').should('contain', "Shop")
     })
 
-    // it("Fill Form using the Home Page form")
+    it.only("Filling the form in the Home Page", function () {
+        //Navigating to the Angular Practice Page
+        cy.visit("https://rahulshettyacademy.com/angularpractice/");
+
+        //Filling the name field of the form
+        cy.get(':nth-child(1) > .form-control').click().type("Batman");
+
+        //Filling the email form of the form
+        cy.get(':nth-child(2) > .form-control').click().type("batman@gotham.com")
+
+        //Filling the Password field of the form
+        cy.get('#exampleInputPassword1').click().type("nananaP@ssword")
+
+        //Selecting the Gender field in the form
+        cy.get('#exampleFormControlSelect1').select("Male");
+
+        //Checking the "Employed" option of the Employment Status section
+        cy.get('#inlineRadio2').check();
+
+        //Filling the Date of Birth field of the form
+        //Facing struggles with this as I could not type the date
+        //Neither could I
+        //cy.get(':nth-child(8) > .form-control').click().type("10101990")
+    })
 })
